9e4627f3e75d5a53792536303bb0cbf0a82ae576
Shadoka <tilmann.groeger@gmx.de>
2020-11-18 13:46:29 +0100

    init - not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client.js');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BACKEND = "http://localhost:8080"
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {subreddits: []};
+  }
+
+  componentDidMount() {
+    console.log("before backend call");
+    client({method: 'GET', path: BACKEND.concat('/subreddits/available')}).then(response => {
+      this.setState({subreddits: response.entity});
+    });
+    console.log("after backend call");
+  }
+
+  storeSubreddits(response) {
+    this.setState({subreddits: response.entity});
+  }
+
+  onButtonClick(endpoint) {
+    alert(endpoint);
+  }
+
+  render() {
+    console.log("in render");
+    const buttons = this.state.subreddits.map(sub => <SubredditButton name={sub} clickMethod={this.onButtonClick}/>);
+
+    return (
+      <div class='button-container'>
+        {buttons}
+      </div>
+    );
+
+    //return (
+    //  <EmployeeList employees={this.state.employees}/>
+    //);
+  }
+
+  createButtonRows() {
+    var rows;
+    var currentRow;
+    
+  }
+}
+
+class SubredditButton extends React.Component {
+  render() {
+    const endpoint = BACKEND.concat("/subreddits/").concat(this.props.name);
+
+    return (
+      <input type='button' onClick={() => this.props.clickMethod(endpoint)} value={this.props.name}/>
+    );
+  }
+
+}
+
+class EmployeeList extends React.Component {
+  render() {
+    const employees = this.props.employees.map(employee =>
+      <Employee key={employee.id} employee={employee}/>  
+    );
+
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Description</th>
+          </tr>
+          {employees}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class Employee extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.employee.firstName}</td>
+        <td>{this.props.employee.lastName}</td>
+        <td>{this.props.employee.description}</td>
+      </tr>
+    );
+  }
 }
 
 export default App;
